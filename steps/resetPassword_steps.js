c2f1229efb773fdc39a7630bc0c07a8976466c7b
Filipv95 <filipv65@gmail.com>
2019-03-13 11:41:05 +0100

    add new step definition

--- steps/resetPassword_steps.js
+++ steps/resetPassword_steps.js
@@ -10,3 +10,16 @@ When('I click Reset button', {timeout: 20000}, async function() {
 Then('I should see Reset password', {timeout: 25000}, async function() {
         await this.resetPassword.checkReset()
 })
+
+When('I entered a {string} that does not exist', {timeout: 20000}, async function(email) {
+        await this.resetPassword.navigateTologIn()
+     
+        await this.resetPassword.reset()
+        await this.resetPassword.emailField.setValue(email)
+     
+        await this.resetPassword.continueButton.click()
+})
+
+Then('I should see pop-up message User does not exist', {timeout: 25000}, async function() {
+        await this.resetPassword.checkWrongMail()
+})
