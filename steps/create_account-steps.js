c4aaf4456d1fc69f73cebaf702d1207ebbe367b2
Nenad Tanaskovic <nenad@fintechdev.net>
2019-04-08 17:12:33 +0200

    EC-297: added get_access_token and open_token_ecd_url methods

--- steps/create_account-steps.js
+++ steps/create_account-steps.js
@@ -1,6 +1,5 @@
 
 const {Given, When, Then} = require('cucumber')
-const basePage = require('../support/pages/base-page')
 const createAccPage = require('../support/pages/create_account-page')
 
 Given('I am on Create Account page of ECD', {timeout: 30000}, async function () { //ToDo: change timeout valie to be a global const
@@ -12,6 +11,6 @@ Then('I should see partners', { timeout: 20000 }, async function () {
 })
 
 Then('I make an API call to create {string} account', { timeout: 20000 }, async function (account) {
-        //ToDo: Hide not needed info, as 'api/user/connectExternal',...
-        await this.basePage.api_call('api/user/connectExternal', [["email"],[account]])
+        let access_token = await this.createAccPage.get_access_token(account)
+        await this.createAccPage.open_token_ecd_url(access_token)
 })
