e64d614d8cfdd1e385081faba06dc346a4366eac
Filipv95 <filipv65@gmail.com>
2019-06-21 14:09:40 +0200

    Change entrypoint.sh for prod enviroment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ npm install
 ./node_modules/.bin/selenium-standalone install
 ./node_modules/.bin/selenium-standalone start > /dev/null 2>&1 &
 sleep 1
-./run.sh dev chrome
+./run.sh prod chrome features/help.feature features/profile.feature features/reset_password.feature features/sign_in.feature features/sign_out.feature features/transaction.feature
