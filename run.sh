38a8059c3d40878c71a64680fedf9f2f5a77ab65
Filipv95 <filipv65@gmail.com>
2019-07-16 17:58:35 +0200

    EC-430: Try to set parametrization for headless

--- run.sh
+++ run.sh
@@ -23,7 +23,9 @@ environment=$1
 environment=${environment:=stage}
 browser=$2
 browser=${browser:=chrome}
+headless=$3
+headless=${headless:=chromeOptions}  #ChromeOptions sam pokupio iz world.js tu su opisane capabilites za headless (to je ponuda resenja za sad)
 
 ./node_modules/.bin/cucumber-js \
-    --world-parameters "{\"environment\": \"${environment}\", \"browser\": \"${browser}\"}" \
+    --world-parameters "{\"environment\": \"${environment}\", \"browser\": \"${browser}\", \"headless\": \"${headless}\"}" \
     ${@:3}
