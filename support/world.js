611eb13543a62bda8dd6025bc2ea8ffeab0d4944
Filipv95 <filipv65@gmail.com>
2019-07-29 15:26:31 +0200

    EC-430: wip

--- support/world.js
+++ support/world.js
@@ -5,19 +5,37 @@ function CustomWorld ({ attach, parameters }) {
   this.attach = attach
   this.parameters = parameters
 
-  const options = {
-    desiredCapabilities: {
-      browserName: 'chrome',
-      chromeOptions: {
-          args: [
-              '--headless',
-              '--no-sandbox',
-              '--disable-gpu'
-          ]
+
+  switch (parameters.browser) {
+    case "chrome":
+    case "firefox":
+      const options = {
+        desiredCapabilities: {
+          browserName: parameters.browser
+        }
       }
-    }
+      this.browser = webdriver.remote(options)
+      break;
+    case "chrome-headless":
+      const options = {
+        desiredCapabilities: {
+          browserName: 'chrome',
+          chromeOptions: {
+              args: [
+                  '--headless',
+                  '--no-sandbox',
+                  '--disable-gpu'
+              ]
+          }
+        }
+      }
+      this.browser = webdriver.remote(options)
+      break;
+    default:
+      console.log('Browser parameter ' + parameters.browser + 'is wrong!')
   }
-  this.browser = webdriver.remote(options)
+
+
 }
 
 setWorldConstructor(CustomWorld)
